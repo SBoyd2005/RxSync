6ddb7c443cf3e17cbaed9d3aa63cdfa96ca5f8a3
Stephen Boyd <Stephen.Boyd48@gmail.com>
2019-04-21 11:53:29 -0500

    Added index.js to pages

--- src/Content.js
+++ src/Content.js
@@ -4,9 +4,9 @@ import * as Pages from './pages';
 export default function Content(state){
     return `
     <div id="content">
-        <div class="container">
-            ${Pages[state.body]()}
-        </div>
+    <div class="container">
+    ${Pages[state.body]()}
     </div>
-`;
+    </div>
+    `;
 }
